da0c467544a0a800ef0071f4df4d43e07464e137
Ramin RX7 <rawmin.rx@gmail.com>
2024-01-25 02:30:13 +0330

    add main.rs

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+use rustx7 as rx;
+
+
+
+const DRAMAX_LOGO : &str = "
+8888888b.                                         Y88b   d88P
+888  \"Y88b                                         Y88b d88P
+888    888                                          Y88o88P
+888    888 888d888 8888b.  88888b.d88b.   8888b.     Y888P
+888    888 888P\"      \"88b 888 \"888 \"88b     \"88b    d888b
+888    888 888    .d888888 888  888  888 .d888888   d88888b
+888  .d88P 888    888  888 888  888  888 888  888  d88P Y88b
+8888888P\"  888    \"Y888888 888  888  888 \"Y888888 d88P   Y88b
+";
+
+
+
+
+fn main() {
+    // rx::test();
+    // rx::exit(0)
+    rx::clear();
+    println!("{}", DRAMAX_LOGO);
+
+
+}
